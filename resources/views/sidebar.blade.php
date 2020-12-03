7b6ef7b43d9a3857dbc38a5e9e7b9ba4ee559acd
Andreas Rizki Rizaldi <arizkirizaldi@email.unikom.ac.id>
2020-12-03 20:31:38 +0700

    data gudang

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Optionally, you can add icons to the links -->
     <li class="{{ Request::is('pengiriman/baru') ? 'active' : '' }}"><a href="{{url('/pengiriman/baru')}}"><i class="fa fa-send"></i> <span>Pengiriman Baru</span></a></li>
     <li class="{{ Request::is('pengiriman/lihatv2') ? 'active' : '' }}"><a href="{{url('/pengiriman/lihatv2')}}"><i class="fa fa-box"></i> <span>Lihat Semua Pengiriman</span></a></li>
-    <li class="{{ Request::is('pengiriman/lihatv2pending') ? 'active' : '' }}"><a href="{{url('/pengiriman/lihatv2pending')}}"><i class="fa fa-box"></i> <span>Lihat Pengiriman Pending</span></a></li>
+    <li class="{{ Request::is('pengiriman/lihatv2pending') ? 'active' : '' }}"><a href="{{url('/pengiriman/lihatv2pending')}}"><i class="fa fa-box"></i> <span>Lihat Data Gudang</span></a></li>
     <li class="header">Kendaraan</li>
     <li class="{{ Request::is('kendaraan/baru') ? 'active' : '' }}"><a href="{{url('/kendaraan/baru')}}"><i class="fa fa-send"></i> <span>Kendaraan Baru</span></a></li>
     <li class="{{ Request::is('kendaraan/lihat') ? 'active' : '' }}"><a href="{{url('/kendaraan/lihat')}}"><i class="fa fa-car"></i> <span>Lihat Semua Kendaraan</span></a></li>
