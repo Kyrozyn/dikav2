de10d52bc909b6c97ea6cfc70fa67acbc58bfc35
Andreas Rizki Rizaldi <arizkirizaldi@email.unikom.ac.id>
2020-12-25 22:43:56 +0700

    faker edit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        pengiriman::factory(10000)->create();
+        pengiriman::factory(1000)->create();
         kendaraan::factory(10)->create();
     }
 }
