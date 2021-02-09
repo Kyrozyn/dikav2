cef48c0ea6ee56da61955a89088b8b10dea1b593
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2021-02-09 20:00:47 +0700

    invoice

--- resources/views/invoice/detail.blade.php
+++ resources/views/invoice/detail.blade.php
@@ -53,11 +53,13 @@
                     </div>
                 </div>
             </div>
+            @if($invoice->status == 'Diterima')
             <div class="row" style="padding-top: 10px">
                 <div class="col-12">
                     <a href="{{url('/laporaninvoice/'.$invoice->id_invoice)}}" class="btn btn-block btn-primary">Cetak Invoice</a>
                 </div>
             </div>
+            @endif
             <div class="row mt-3">
                 <div class="col-12">
                     <table id="table_id" class="display">
